7f23f96802cd3a03d809ed1a8b6cb09c9cff6560
shontzu <shon.tzu@besquare.com.my>
2022-07-14 22:41:57 +0800

    STEP ONE 	fetch endpoint and console.log the response

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [posts, setPosts] = React.useState();
+  const [comments, setComments] = React.useState();
+
+  //connect to endpoint
+  //GET POSTS (METHOD #1: PROMISE)
+  async function getPosts() {
+    let getPosts = await fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(response => {console.log(response);return response.json()})
+      .then(json => {console.log(json); return setPosts(json)})
+
+    if (getPosts) {
+      console.log(posts);
+    } 
+  }
+
+  //GET COMMENTS (METHOD #2: ASYNC-AWAIT)
+  async function getComments() {
+    let getComments = await fetch("https://jsonplaceholder.typicode.com/comments");
+    console.log(getComments);
+
+    let json = await getComments.json();
+    console.log(json);
+
+    if (getComments) {
+      setComments(json);
+      console.log(comments);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={getPosts}>GET POSTS</button>
+        <button onClick={getComments}>GET COMMENTS</button>
+        {/* <div>
+          {post.map(function (key, index) {
+            return (
+              <div key={index}>
+                <p>Name: {key.name}</p>
+                <p>email: {key.email}</p>
+                <p>body: {key.body}</p>
+                <hr />
+              </div>
+            );
+          })}
+        </div> */}
       </header>
     </div>
   );
